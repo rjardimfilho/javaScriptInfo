66cacc365340507795ee9c3513d2e32d78c99256
Renato Jardim <rjardimfilho@gmail.com>
2019-05-14 19:25:31 -0400

    javaScript Variables

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict'
+// Working with variables
+let admin
+let name = "John";
+admin = name;
+alert(admin);
+
+// Giving the rigth name
+let ourPlanetNme = earth;
+let currentUserName = "John";
+
+//Uppercase const?
+const BIRTHDAY = '18.04.1982'; //Ok, because the date of birthday is a known constant.
+constant AGE = someCode(BIRTHDAY); //Not ok to be UPPERCASE because age changes every year. 
